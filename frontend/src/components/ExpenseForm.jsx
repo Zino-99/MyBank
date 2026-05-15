e0ddbb06cd4fe982b03842d845012b9e42bd795d
DJADEL <djadel.vfc@gmail.com>
2026-05-15 14:09:46 +0200

    fix Ci ans test

---
+++ frontend/src/components/ExpenseForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ExpenseForm() {
+  return (
+    <form>
+      <label>
+        Amount
+        <input />
+      </label>
+
+      <button>Add</button>
+    </form>
+  );
+}
