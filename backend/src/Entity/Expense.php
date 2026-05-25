50a33b1586edd665abf8b17a1ac9191924304b4a
DJADEL <djadel.vfc@gmail.com>
2026-05-26 01:42:36 +0200

    ci: ajoute PHPStan dans la CI

--- backend/src/Entity/Expense.php
+++ backend/src/Entity/Expense.php
@@ -12,6 +12,7 @@ class Expense
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+/** @phpstan-ignore property.unusedType */
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
--- backend/tests/bootstrap.php
+++ backend/tests/bootstrap.php
@@ -3,9 +3,7 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 $_ENV['APP_ENV'] = 'test';
 $_SERVER['APP_ENV'] = 'test';
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
